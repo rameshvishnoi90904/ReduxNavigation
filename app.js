8905e378af90fc19972ec7f04ad58c60c6dbc816
Ramesh Vishnoi <rameshvishnoi90904@gmail.com>
2017-07-04 20:57:45 +0530

    common code entry point from app.js

--- app.js
+++ app.js
@@ -1,82 +1,20 @@
 
-import React from 'react'
-import { TouchableHighlight, View, Text, StyleSheet } from 'react-native'
+import React from 'react';
+import { AppRegistry } from 'react-native';
+import { Provider } from 'react-redux';
+import { createStore } from 'redux';
 
-import { connect } from 'react-redux'
-import { getData } from './src/actions/action'
+import AppReducer from './src/reducers';
+import AppWithNavigationState from './src/navigators/AppNavigator';
 
+export default class ReduxExampleApp extends React.Component {
+  store = createStore(AppReducer);
 
-let styles
-
-const App = (props) => {
-  const {
-    container,
-    text,
-    button,
-    buttonText,
-    mainContent
-  } = styles
-
-  return (
-    <View style={container}>
-      <Text style={text}>Redux Examples</Text>
-      <TouchableHighlight style={button} onPress={() => props.fetchData()}>
-        <Text style={buttonText}>Load Data</Text>
-      </TouchableHighlight>
-      <View style={mainContent}>
-      {
-        props.appData.isFetching && <Text>Loading</Text>
-      }
-      {
-        props.appData.data.length ? (
-          props.appData.data.map((person, i) => {
-            return <View key={i} >
-              <Text>Name: {person.name}</Text>
-              <Text>Age: {person.age}</Text>
-            </View>
-          })
-        ) : null
-      }
-      </View>
-    </View>
-  )
-}
-
-styles = StyleSheet.create({
-  container: {
-    marginTop: 100
-  },
-  text: {
-    textAlign: 'center'
-  },
-  button: {
-    height: 60,
-    margin: 10,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#0b7eff'
-  },
-  buttonText: {
-    color: 'white'
-  },
-  mainContent: {
-    margin: 10,
-  }
-})
-
-function mapStateToProps (state) {
-  return {
-    appData: state.appData
+  render() {
+    return (
+      <Provider store={this.store}>
+        <AppWithNavigationState />
+      </Provider>
+    );
   }
 }
-
-function mapDispatchToProps (dispatch) {
-  return {
-    getData: () => dispatch(getData())
-  }
-}
-
-export default connect(
-  mapStateToProps,
-  mapDispatchToProps
-)(App)
--- index.android.js
+++ index.android.js
@@ -4,24 +4,9 @@
 
 import React from 'react';
 import { AppRegistry } from 'react-native';
-import { Provider } from 'react-redux';
-import { createStore } from 'redux';
 
-import AppReducer from './src/reducers';
-import AppWithNavigationState from './src/navigators/AppNavigator';
+import App from './app';
 
-class ReduxExampleApp extends React.Component {
-  store = createStore(AppReducer);
+AppRegistry.registerComponent('AwesomeProject', () => App);
 
-  render() {
-    return (
-      <Provider store={this.store}>
-        <AppWithNavigationState />
-      </Provider>
-    );
-  }
-}
-
-AppRegistry.registerComponent('AwesomeProject', () => ReduxExampleApp);
-
-export default ReduxExampleApp;
+export default App;
